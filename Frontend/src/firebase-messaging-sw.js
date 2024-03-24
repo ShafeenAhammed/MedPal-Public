a80334f557197b0e6943dd8991e7c9ca6ddae790
ShafeenAhammed <ahammedshafeen10@gmail.com>
2024-03-24 14:39:16 +0530

    removing api key

--- Frontend/src/firebase-messaging-sw.js
+++ Frontend/src/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCVMBoPf0oZgJIsilHkXUiwEjLnw_oxI9A",
-    authDomain: "medpal-5ae11.firebaseapp.com",
-    projectId: "medpal-5ae11",
-    storageBucket: "medpal-5ae11.appspot.com",
-    messagingSenderId: "729366534377",
-    appId: "1:729366534377:web:9d302b52e9e26ea637f878",
-    measurementId: "G-M60P9W7SM3"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 const messaging = firebase.messaging();
